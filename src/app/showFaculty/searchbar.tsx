4d6756a37a34bad5c7fd42b929cb9e5e84e3f6b6
Sarath191181208 <vssarathc04@gmail.com>
2024-06-29 23:44:14 +0530

    search bar color and invalid props fix

--- src/app/showFaculty/searchbar.tsx
+++ src/app/showFaculty/searchbar.tsx
@@ -17,16 +17,16 @@ export function SearchBar({
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
         </div>
 
         <input
-          className="peer h-full w-full outline-none bg-slate-800 text-sm text-gray-700 px-2"
+          className="peer h-full w-full outline-none bg-slate-800 text-sm text-white-700 px-2"
           type="text"
           id="search"
           onChange={onChange}
